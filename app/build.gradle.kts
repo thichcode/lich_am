2723785e975db4a71415132efb214ab0ab3acb29
MinhTran <designbyanchi@gmail.com>
2026-07-05 13:06:40 +0700

    Bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.licham"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildFeatures {
