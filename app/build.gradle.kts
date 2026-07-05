b04dfcdaa1b39ee8efa1145f500954360edb0adc
MinhTran <designbyanchi@gmail.com>
2026-07-05 23:33:25 +0700

    Add Firebase Test Lab workflow + UI screenshot instrumentation test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,10 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
 }
---
+++ app/src/androidTest/java/com/licham/ScreenshotTest.kt
@@ -0,0 +1,58 @@
+package com.licham
+
+import android.Manifest
+import android.os.Environment
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import androidx.test.uiautomator.UiDevice
+
+@RunWith(AndroidJUnit4::class)
+class ScreenshotTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    @Test
+    fun takeScreenshots() {
+        val device = UiDevice.getInstance(getInstrumentation())
+        val screenshotDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+            "screenshots"
+        )
+        screenshotDir.mkdirs()
+
+        // Screenshot 1: Home screen
+        takeScreenshot(device, screenshotDir, "home_screen")
+        
+        // Open calendar tab
+        onView(withText("Lịch")).perform(androidx.test.espresso.action.ViewActions.click())
+        takeScreenshot(device, screenshotDir, "calendar_screen")
+        
+        // Open good days tab
+        onView(withText("Ngày đẹp")).perform(androidx.test.espresso.action.ViewActions.click())
+        takeScreenshot(device, screenshotDir, "good_days_screen")
+        
+        // Open news tab
+        onView(withText("Tin tức")).perform(androidx.test.espresso.action.ViewActions.click())
+        takeScreenshot(device, screenshotDir, "news_screen")
+    }
+
+    private fun takeScreenshot(device: UiDevice, directory: File, name: String) {
+        val filename = "${name}_${System.currentTimeMillis()}.png"
+        device.takeScreenshot(File(directory, filename))
+    }
+}
